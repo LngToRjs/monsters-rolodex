63a98942d5e47cbdd82ae9276030880926a278d4
LngToRjs <59631333+LngToRjs@users.noreply.github.com>
2020-01-08 16:43:39 -0600

    Updates

---
+++ precache-manifest.0a20da370eb1b2b18a0c8f6f5a1d612c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7e2b89ae0e006f4731708cb1f99052",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d200574335265646edb",
+    "url": "/monsters-rolodex/static/css/main.97ad187c.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d200574335265646edb",
+    "url": "/monsters-rolodex/static/js/main.649f1dc1.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
